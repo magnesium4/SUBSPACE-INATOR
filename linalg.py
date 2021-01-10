dbc40e52b41d2485fc33d85821bc731af1cc9ad8
Maryam Gohargani <maryam.gohargani@mail.utoronto.ca>
2021-01-10 11:13:27 -0500

    initial python lin alg file

---
+++ linalg.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def findNullSpace(A: List[List[int]]) -> List[List[int]]:
+    """
+    Takes in a NxN matrix A and returns a basis for its null space.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print("hi")
