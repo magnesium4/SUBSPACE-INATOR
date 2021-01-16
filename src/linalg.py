2bf747a8a43684135f3e7e3c353736aa507b0d00
Maryam Gohargani <maryam.gohargani@mail.utoronto.ca>
2021-01-15 19:40:09 -0500

    added the main functions

--- src/linalg.py
+++ src/linalg.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy import linalg as LA
 from scipy.linalg import null_space
 from copy import deepcopy
+from sympy import Matrix
 
 def find_null_space(A: List[List[float]]) -> List[List[float]]:
     """
@@ -41,6 +42,7 @@ def is_zero(A: List[List[float]]) -> bool:
     return True
 
 
+# TODO: not sure if it works
 def find_generalized_eigen_basis(A: List[List[float]], x: float) -> List[float]:
     """
     Takes in a square matrix A and an eigen value and returns the corresponding generalized eigen basis.
@@ -51,7 +53,43 @@ def find_generalized_eigen_basis(A: List[List[float]], x: float) -> List[float]:
     while not is_zero(B):
         generalized_eigens.extend(find_eigen_basis(B, x))
         B = np.matmul(np.array(B), np.array(A)).tolist()
+    return generalized_eigens
 
+
+# TODO: not sure if it works
+def find_jordan_basis(A: List[List[float]]) -> List[List[float]]:
+    """
+    Takes in a square matrix A and returns its jordan basis.
+    """
+    eigens = find_eigen_vals(A)
+    basis = []
+    for e in eigens:
+        B = deepcopy(A)
+        generalized_eigens = []
+        while not is_zero(B):
+            generalized_eigens.append(find_eigen_basis(B, x))
+            B = np.matmul(np.array(B), np.array(A)).tolist()
+    
+
+def find_jordan_form(A: List[List[float]]) -> List[List[float]]:
+    """
+    Returns the Jordan form of a matrix A
+    """
+    m = Matrix(np.array(A))
+    return(np.array(m.jordan_form()[1]).tolist())
+
+
+def count_inv_subspace(A: List[List[float]]) -> int:
+    """
+    Returns the number of invariant subspaces (jordan blocks) in A
+    """
+    J = find_jordan_form(A)
+    ans = 1
+    for i in range(1, len(J)):
+        if J[i - 1][i] == 0:
+            ans += 1
+    return ans
 
 if __name__ == "__main__":
-    print("hi")
+    print(find_jordan_form([[5, 4, 2, 1], [0, 1, -1, -1], [-1, -1, 3, 0], [1, 1, -1, 2]]))
+    print(count_inv_subspace([[5, 4, 2, 1], [0, 1, -1, -1], [-1, -1, 3, 0], [1, 1, -1, 2]]))
